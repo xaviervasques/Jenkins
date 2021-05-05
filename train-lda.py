5c82f62de2203129947c341a27dcca6b878f3cf4
root <root@xavi.home>
2021-05-05 14:05:15 +0200

    modified:   train-lda.py

--- train-lda.py
+++ train-lda.py
@@ -10,7 +10,6 @@ import scipy
 import os
 import numpy as np
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-#from sklearn.neural_network import MLPClassifier
 import pandas as pd
 from joblib import dump, load
 from sklearn import preprocessing
@@ -21,9 +20,7 @@ def train():
 
     MODEL_DIR = os.environ["MODEL_DIR"]
     MODEL_FILE_LDA = os.environ["MODEL_FILE_LDA"]
-#    MODEL_FILE_NN = os.environ["MODEL_FILE_NN"]
     MODEL_PATH_LDA = os.path.join(MODEL_DIR, MODEL_FILE_LDA)
-#    MODEL_PATH_NN = os.path.join(MODEL_DIR, MODEL_FILE_NN)
 
     # Load, read and normalize training data
     training = "./train.csv"
@@ -31,11 +28,7 @@ def train():
 
     y_train = data_train['# Letter'].values
     X_train = data_train.drop(data_train.loc[:, 'Line':'# Letter'].columns, axis = 1)
-
-    #print("Shape of the training data")
-    #print(X_train.shape)
-   # print(y_train.shape)
-
+    
     # Data normalization (0,1)
     X_train = preprocessing.normalize(X_train, norm='l2')
 
@@ -56,33 +49,13 @@ def train():
     y_test = data_test['# Letter'].values
     X_test = data_test.drop(data_test.loc[:, 'Line':'# Letter'].columns, axis = 1)
 
-  #  print("Shape of the test data")
-  # print(X_test.shape)
-  #  print(y_test.shape)
-
     # Data normalization (0,1)
     X_test = preprocessing.normalize(X_test, norm='l2')
 
     # load and Run model
-    #print(MODEL_PATH_LDA)
     clf_lda = load(MODEL_PATH_LDA)
-    #print("LDA score and classification:")
-    #print('Accuracy = %.3f' % (clf_lda.score(X_test, y_test)))
-    #print((clf_lda.score(X_test, y_test)))
-    #accuracy = clf_lda.score(X_test, y_test)[0]
-    #accuracy = int(np.accuracy)
-    #print('%i'%accuracy)
-    #print(clf_lda.predict(X_test))
     
-    # Print in a created accuracy.txt file the accuaracy score of our model
-    #filename = MODEL_DIR+'accuracy.txt'
-    #with open('accuracy.txt','w') as f:
-    print('%.3f' % (clf_lda.score(X_test, y_test)))
-   
-    #filename = MODEL_DIR+'result.txt'
-    #file = open(filename,'w')
-    #file.write(str(clf_lda.score(X_test, y_test)))
-
+    print(int(clf_lda.score(X_test, y_test)*100))
 
 if __name__ == '__main__':
     train()
